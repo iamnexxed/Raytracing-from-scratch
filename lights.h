f88a0f6876bb61cb1e0c7282f8956eeadbab4dcb
// Sudhanshu <keysatmyfingertips@yahoo.com>
2022-02-01 19:23:46 +0530

    Added shadows

--- lights.h
+++ lights.h
@@ -41,34 +41,6 @@ double ComputeLighting(Vector3 point, Vector3 normal)
 {
     double intensity = 0.0;
     Vector3 L(0, 0, 0);
-    for(int i = 0; i < NUMOFLIGHTS; ++i)
-    {
-        if(lightSources[i].type == Ambient)
-        {
-            intensity += lightSources[i].intensity;
-        }
-        else if (lightSources[i].type == Point)
-        {
-            L = lightSources[i].position - point;
-        }
-        else if (lightSources[i].type == Directional)
-        {
-            L = lightSources[i].direction;
-        }
-        double nDotL = DotProduct(normal, L);
-        if(nDotL > 0)
-        {
-            intensity += lightSources[i].intensity * nDotL / (normal.Magnitude() * L.Magnitude());
-        }
-    }
-    return intensity;
-}
-
-// Absorption and reflection
-double ComputeLighting(Vector3 point, Vector3 normal, Vector3 viewVector, double spec)
-{
-    double intensity = 0.0;
-    Vector3 L(0, 0, 0);
     for (int i = 0; i < NUMOFLIGHTS; ++i)
     {
         if (lightSources[i].type == Ambient)
@@ -84,6 +56,100 @@ double ComputeLighting(Vector3 point, Vector3 normal, Vector3 viewVector, double
             L = lightSources[i].direction;
         }
         double nDotL = DotProduct(normal, L);
+        if (nDotL > 0)
+        {
+            intensity += lightSources[i].intensity * nDotL / (normal.Magnitude() * L.Magnitude());
+        }
+    }
+    return intensity;
+}
+
+Vector2 IntersectRaySphere(Vector3 rayOrigin, Vector3 rayDirection, Sphere &sphere)
+{
+    Vector2 t(-INFINITY, -INFINITY);
+
+    // (t2D, D) + t(2CO, D) + (CO, CO) − r2 = 0
+
+    double a = DotProduct(rayDirection, rayDirection);
+
+    double b = 2 * DotProduct(rayOrigin - sphere.center, rayDirection);
+
+    double c = DotProduct(rayOrigin - sphere.center, rayOrigin - sphere.center) - sphere.radius * sphere.radius;
+
+    double discriminant = b * b - 4 * a * c;
+
+    if (discriminant > 0)
+    {
+        t.x = (-b + sqrt(discriminant)) / (2 * a);
+        t.y = (-b - sqrt(discriminant)) / (2 * a);
+    }
+
+    return t;
+}
+
+Sphere* ClosestSphereIntersection(Vector3 &origin, Vector3 &direction, double tMin, double tMax, Sphere *spheres, const int NUM_SPHERES, double &closestT)
+{
+    Sphere *closestSphere = nullptr;
+    closestT = INFINITY;
+
+    for (int i = 0; i < NUM_SPHERES; i++)
+    {
+        Vector2 t = IntersectRaySphere(origin, direction, spheres[i]);
+        if (t.x >= tMin && t.x <= tMax && t.x < closestT)
+        {
+            closestT = t.x;
+            closestSphere = &spheres[i];
+        }
+
+        if (t.y >= tMin && t.y <= tMax && t.y < closestT)
+        {
+            closestT = t.y;
+            closestSphere = &spheres[i];
+        }
+    }
+
+    return closestSphere;
+}
+
+// Absorption and reflection and shadows
+double ComputeLighting(Vector3 point, Vector3 normal, Vector3 viewVector, double spec, Sphere *spheres, const int NUM_SPHERES)
+{
+    double intensity = 0.0;
+    Vector3 L(0, 0, 0);
+
+    double tMax = 0;
+
+    for (int i = 0; i < NUMOFLIGHTS; ++i)
+    {
+        if (lightSources[i].type == Ambient)
+        {
+            intensity += lightSources[i].intensity;
+        }
+        else if (lightSources[i].type == Point)
+        {
+            L = lightSources[i].position - point;
+            tMax = 1;
+        }
+        else if (lightSources[i].type == Directional)
+        {
+            L = lightSources[i].direction;
+            tMax = INFINITY;
+        }
+
+        // Shadow check
+        if (tMax > 0)
+        {
+            double shadowT = 0;
+            Sphere *shadowSphere = ClosestSphereIntersection(point, L, 0.001, tMax, spheres, NUM_SPHERES, shadowT);
+            
+            
+            if (shadowSphere != nullptr)
+            {
+                continue;
+            }
+        }
+
+        double nDotL = DotProduct(normal, L);
         // Diffuse lighting
         if (nDotL > 0)
         {
@@ -91,11 +157,11 @@ double ComputeLighting(Vector3 point, Vector3 normal, Vector3 viewVector, double
         }
 
         // Specular lighting
-        if(spec > 0)
+        if (spec > 0)
         {
-            Vector3 R = normal * DotProduct(normal, L) * 2  - L;
+            Vector3 R = normal * DotProduct(normal, L) * 2 - L;
             double rDotV = DotProduct(R, viewVector);
-            if(rDotV > 0)
+            if (rDotV > 0)
             {
                 intensity += lightSources[i].intensity * std::pow(rDotV / (R.Magnitude() * viewVector.Magnitude()), spec);
             }
@@ -103,3 +169,71 @@ double ComputeLighting(Vector3 point, Vector3 normal, Vector3 viewVector, double
     }
     return intensity;
 }
+
+
+Vector3 TraceRayOnSpheres(Vector3 rayOrigin, Vector3 rayDirection, double tMin, double tMax, Sphere *spheres, const int NUM_SPHERES)
+{
+    Vector3 color = BLACK;
+
+    double closestT = tMax;
+
+    Sphere *closestSphere = nullptr;
+
+    for (int i = 0; i < NUM_SPHERES; i++)
+    {
+        Vector2 t = IntersectRaySphere(rayOrigin, rayDirection, spheres[i]);
+
+        if (t.x >= tMin && t.x <= tMax && t.x < closestT)
+        {
+            closestT = t.x;
+            closestSphere = &spheres[i];
+        }
+
+        if (t.y >= tMin && t.y <= tMax && t.y < closestT)
+        {
+            closestT = t.y;
+            closestSphere = &spheres[i];
+        }
+    }
+
+    if (closestSphere != nullptr)
+    {
+
+        Vector3 P = rayOrigin + rayDirection * closestT;
+        Vector3 N = P - closestSphere->center;
+        N.Normalize();
+
+        color = closestSphere->color * ComputeLighting(P, N, -rayDirection, closestSphere->specular, spheres, NUM_SPHERES);
+
+        // color = closestSphere->color * ComputeLighting(P, N);
+        // color = closestSphere->color;
+    }
+
+    return color;
+}
+
+Vector3 TraceRayOnObjects(Vector3 rayOrigin, Vector3 rayDirection, double tMin, double tMax, Sphere *spheres, const int NUM_SPHERES)
+{
+    double closestT = 0;
+
+    Sphere *closestSphere = ClosestSphereIntersection(rayOrigin, rayDirection, tMin, tMax, spheres, NUM_SPHERES, closestT);
+    
+
+    Vector3 color = BLACK;
+
+    if (closestSphere != nullptr)
+    {
+
+        Vector3 P = rayOrigin + rayDirection * closestT;
+        Vector3 N = P - closestSphere->center;
+        N.Normalize();
+
+        color = closestSphere->color * ComputeLighting(P, N, -rayDirection, closestSphere->specular, spheres, NUM_SPHERES);
+
+        // color = closestSphere->color * ComputeLighting(P, N);
+        // color = closestSphere->color;
+    }
+
+    return color;
+}
+
--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ void LoadSpheres()
     // Green
     if(i < NumOfSpheres)
     {
-        spheres[i].center = Vector3(-50, 0, 1000);
+        spheres[i].center = Vector3(100, 0, 900);
         spheres[i].radius = 70;
         spheres[i].color = Vector3(0, 1, 0);
         spheres[i].specular = 50;
@@ -23,7 +23,7 @@ void LoadSpheres()
     // Blue
     if (i < NumOfSpheres)
     {
-        spheres[i].center = Vector3(100, 70, 1000);
+        spheres[i].center = Vector3(200, 70, 900);
         spheres[i].radius = 100;
         spheres[i].color = Vector3(0, 0, 1);
         spheres[i].specular = 0;
@@ -32,7 +32,7 @@ void LoadSpheres()
     // Red
     if (i < NumOfSpheres)
     {
-        spheres[i].center = Vector3(0, 300, 1000);
+        spheres[i].center = Vector3(-100, 0, 800);
         spheres[i].radius = 100;
         spheres[i].color = Vector3(1, 0, 0);
         spheres[i].specular = 30;
@@ -42,79 +42,14 @@ void LoadSpheres()
     // White
     if (i < NumOfSpheres)
     {
-        spheres[i].center = Vector3(0, -100, 1000);
-        spheres[i].radius = 100;
+        spheres[i].center = Vector3(0, -1050, 1000);
+        spheres[i].radius = 1000;
         spheres[i].color = Vector3(1, 1, 1);
-        spheres[i].specular = 10;
+        spheres[i].specular = 100;
         i++;
     }
 }
 
-Vector2 IntersectRaySphere(Vector3 rayOrigin, Vector3 rayDirection, Sphere &sphere)
-{
-    Vector2 t(-INFINITY, -INFINITY);
-
-    // (t2D, D) + t(2CO, D) + (CO, CO) − r2 = 0
-
-    double a = DotProduct(rayDirection, rayDirection);
-
-    double b = 2 * DotProduct(rayOrigin - sphere.center, rayDirection);
-
-    double c = DotProduct(rayOrigin - sphere.center, rayOrigin - sphere.center) - sphere.radius * sphere.radius;
-
-    double discriminant = b * b - 4 * a * c;
-
-    if (discriminant > 0)
-    {
-        t.x = (-b + sqrt(discriminant)) / (2 * a);
-        t.y = (-b - sqrt(discriminant)) / (2 * a);
-    }
-
-    return t;
-}
-
-Vector3 TraceRayOnSpheres(Vector3 rayOrigin, Vector3 rayDirection, double tMin, double tMax, Sphere *spheres, const int NUM_SPHERES)
-{
-    Vector3 color = BLACK;
-
-    double closestT = tMax;
-
-    Sphere *closestSphere = nullptr;
-
-    for (int i = 0; i < NUM_SPHERES; i++)
-    {
-        Vector2 t = IntersectRaySphere(rayOrigin, rayDirection, spheres[i]);
-
-        if (t.x >= tMin && t.x <= tMax && t.x < closestT)
-        {
-            closestT = t.x;
-            closestSphere = &spheres[i];
-        }
-
-        if (t.y >= tMin && t.y <= tMax && t.y < closestT)
-        {
-            closestT = t.y;
-            closestSphere = &spheres[i];
-        }
-    }
-
-    if (closestSphere != nullptr)
-    {
-
-        Vector3 P = rayOrigin + rayDirection * closestT;
-        Vector3 N = P - closestSphere->center;
-        N.Normalize();
-
-        color = closestSphere->color * ComputeLighting(P, N, -rayDirection, closestSphere->specular);
-
-        //color = closestSphere->color * ComputeLighting(P, N);
-        //color = closestSphere->color;
-    }
-
-    
-
-    return color;
-}
 
 void DrawPixel(int x, int y, GLfloat r, GLfloat g, GLfloat b)
 {
@@ -138,20 +73,19 @@ void TraceSpheres()
         {
             Vector2Int canvas(x, y);
             Vector3 Dir = CanvasToViewport(canvas);
-            Vector3 color = TraceRayOnSpheres(Origin, Dir, 1, INFINITY, spheres, NumOfSpheres);
-            
+            //Vector3 color = TraceRayOnSpheres(Origin, Dir, 1, INFINITY, spheres, NumOfSpheres);
+            Vector3 color = TraceRayOnObjects(Origin, Dir, 1, INFINITY, spheres, NumOfSpheres);
+
             DrawPixel(x, y, color.x, color.y, color.z);
         }
     }
 }
 
-// Clears the current window and draws a triangle.
+// Clears the current window 
 void display()
 {
     // Set every pixel in the frame buffer to the current clear color.
     glClear(GL_COLOR_BUFFER_BIT);
-
-    //DrawPixel(0, -200, 1, 1, 1);
     
     TraceSpheres();
     // Flush drawing command buffer to make drawing happen as soon as possible.
